68b15e0004369059406138820923a806b8db08f3
Dimitri Lhopital <dimitrilho@gmail.com>
2023-03-08 11:45:29 +0400

    feat: ✨Add header to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import tw from 'twin.macro'
+import './App.css'
+import { regionsList } from './commons/constants'
+import Header from './components/Header'
+import RegionList from './components/Regionlist'
+import Searchbar from './components/Searchbar'
+import { useTheme } from './context/themeContext'
+import { Theme } from './types/types'
+
+const AppContainer = styled.div<{ customTheme: Theme }>`
+  background-color: ${(props) => props.customTheme.backgroundP};
+  color: ${(props) => props.customTheme.foreground};
+
+  ${tw`
+			min-h-screen
+	`}
+`
+const FiltersContainer = styled.div<{ customTheme: Theme }>`
+  background-color: ${(props) => props.customTheme.backgroundP};
+
+  ${tw`
+			flex
+			flex-row
+			sm:flex-row
+			justify-between
+			px-10
+			h-16
+			my-24
+			md:my-10
+	`}
+`
 
 function App() {
+  const { theme } = useTheme()
+  const [search, setSearch] = React.useState<string>('')
+  const [region, setRegion] = React.useState<string>(regionsList[0])
+
+  const handleSearchChange = (value: string): void => {
+    setSearch(value)
+  }
+  const handleRegion = (value: string): void => {
+    setRegion(value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppContainer customTheme={theme}>
+      <Header />
+    </AppContainer>
+  )
 }
 
-export default App;
+export default App
