55e7a59777ea561fd30a0c6b41c982a87f3b65f4
Dimitri Lhopital <dimitrilho@gmail.com>
2023-03-08 09:59:15 +0400

    feat: ✨ Add types children and theme in srting for storage

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,11 @@
 export type Theme = {
-    backgroundP: string,
-    backgroundSd: string,
-    foreground: string,
-    iconColor: string
+  backgroundP: string
+  backgroundSd: string
+  foreground: string
+  iconColor: string
 }
+
+export type ChildrenProps = {
+  children: React.ReactNode
+}
+export type ThemeString = 'light' | 'dark'
