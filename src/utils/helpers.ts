e469fc42eaec74deb731d7eba07f2eee07612132
Dimitri Lhopital <dimitrilho@gmail.com>
2023-03-09 18:22:05 +0400

    fix: 🐛 test useless in TS

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,15 +2,9 @@ import { darkTheme, lightTheme } from '../commons/constants'
 import { Theme, ThemeString } from '../types/types'
 
 export const getTheme = (text: ThemeString): Theme => {
-  if (!text) {
-    throw new Error(`le texte n'est pas renseigner pour ${getTheme.name}`)
-  }
   return text === 'light' ? lightTheme : darkTheme
 }
 
 export const getThemeString = (theme: Theme): ThemeString => {
-  if (!theme) {
-    throw new Error(`le thème n'est pas renseigner pour ${getThemeString.name}`)
-  }
   return theme === lightTheme ? 'light' : 'dark'
 }
